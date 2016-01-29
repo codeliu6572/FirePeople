0b8123a15b59bf1360c1ea9e7cc2cadc8806ad6f
codeliu6572 <codeliu6572@163.com>
2016-01-29 15:02:30 +0800

    Initial Commit

---
+++ 火柴人/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  火柴人
+//
+//  Created by 刘浩浩 on 16/1/29.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 火柴人/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  火柴人
+//
+//  Created by 刘浩浩 on 16/1/29.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
